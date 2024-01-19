2183e2f2f77e7d9829c7c45b801ceff117a960fb
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-19 13:30:55 +0100

    add: initial backend code

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello World!' });
+});
+
+app.listen(port, () => {
+	console.log('Listening on port: ' + port);
+});
