25b851570924802bffc6745fa033a12966a9adde
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-23 22:18:05 +0100

    add: loading spinner and fetch failed error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,28 @@ import * as NotesApi from './network/notes_api';
 import logo from './assets/remind-me-logo.png';
 import AddEditNoteModal from './components/AddEditNoteModal';
 import { ReactComponent as Plus } from './assets/plus.svg';
+import { MutatingDots } from 'react-loader-spinner';
 
 function App() {
 	const [notes, setNotes] = useState<INote[]>([]);
+	const [notesLoading, setNotesLoading] = useState(true);
+	const [showNotesLoadingError, setShowNotesLoadingError] = useState(false);
 	const [showAddNoteModal, setShowAddNoteModal] = useState(false);
 	const [noteToEdit, setNoteToEdit] = useState<INote | null>(null);
 
 	useEffect(() => {
 		const getNotes = async () => {
 			try {
+				setNotesLoading(true);
+				setShowNotesLoadingError(false);
 				const notes = await NotesApi.fetchNotes();
 
 				setNotes(notes);
 			} catch (error) {
 				console.error(error);
+				setShowNotesLoadingError(true);
+			} finally {
+				setNotesLoading(false);
 			}
 		};
 
@@ -68,17 +76,33 @@ function App() {
 				<Plus className='plus' />
 				Add Note
 			</button>
-			<NoteList>
-				{notes.map((note) => (
-					<Note
-						key={note._id}
-						note={note}
-						onDeleteNoteClicked={deleteNote}
-						setNoteToEdit={(note: INote) => setNoteToEdit(note)}
-						showAddNoteModal={() => setShowAddNoteModal(true)}
-					/>
-				))}
-			</NoteList>
+			{notesLoading && (
+				<MutatingDots
+					visible={true}
+					height='100'
+					width='100'
+					color='#bf5f8d'
+					secondaryColor='#bf5f8d'
+					radius='12.5'
+					ariaLabel='mutating-dots-loading'
+					wrapperStyle={{}}
+					wrapperClass=''
+				/>
+			)}
+			{showNotesLoadingError && <p className='error'>Something went wrong!</p>}
+			{!notesLoading && !showNotesLoadingError && (
+				<NoteList>
+					{notes.map((note) => (
+						<Note
+							key={note._id}
+							note={note}
+							onDeleteNoteClicked={deleteNote}
+							setNoteToEdit={(note: INote) => setNoteToEdit(note)}
+							showAddNoteModal={() => setShowAddNoteModal(true)}
+						/>
+					))}
+				</NoteList>
+			)}
 		</div>
 	);
 }
