3edf2b4aa062a12ec01cdc9bd800f7fd590fe057
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-25 20:56:40 +0100

    add: toggling between register or login action

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
 import AuthScreen from './components/AuthScreen';
 import { User } from './models/user';
 import NavBar from './components/NavBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import LoggedInView from './components/LoggedInView';
+import * as UsersApi from './network/users_api';
 
 function App() {
 	const [authenticatedUser, setAutheticatedUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		async function fetchLoggedInUser() {
+			try {
+				const user = await UsersApi.getLoggedInUser();
+
+				setAutheticatedUser(user);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+		fetchLoggedInUser();
+	}, []);
 
 	return (
 		<div className='App'>
@@ -18,7 +32,6 @@ function App() {
 				<LoggedInView />
 			) : (
 				<AuthScreen
-					register={true}
 					onSuccessfulAuthentication={(user) => setAutheticatedUser(user)}
 				/>
 			)}
--- frontend/src/components/AuthScreen.tsx
+++ frontend/src/components/AuthScreen.tsx
@@ -6,23 +6,23 @@ import { ReactComponent as Eye } from '../assets/eye.svg';
 import { isValidEmail } from '../utils/isValidEmail';
 
 interface Props {
-	register: boolean;
 	onSuccessfulAuthentication: (user: User) => void;
 }
 
-const AuthModal = ({ register, onSuccessfulAuthentication }: Props) => {
+const AuthModal = ({ onSuccessfulAuthentication }: Props) => {
 	const [username, setUsername] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [revealPassword, setRevealPassword] = useState(false);
 	const [isFetching, setIsFetching] = useState(false);
 	const [invalidEmailError, setInvalidEmailError] = useState(false);
+	const [registerMode, setRegisterMode] = useState(false);
 
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setIsFetching(true);
 		try {
-			if (register) {
+			if (registerMode) {
 				if (!isValidEmail(email)) {
 					setInvalidEmailError(true);
 					return;
@@ -43,62 +43,73 @@ const AuthModal = ({ register, onSuccessfulAuthentication }: Props) => {
 	};
 
 	return (
-		<form className={styles.authScreen} onSubmit={(e) => handleSubmit(e)}>
-			<h2 className={styles.authScreenHeader}>
-				{register ? 'Register' : 'Login'}
-			</h2>
-			<div className={styles.authScreenInput}>
-				<label htmlFor='username'>Username:</label>
-				<input
-					className={styles.authScreenInput}
-					value={username}
-					type='text'
-					id='username'
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-			</div>
-			{register && (
+		<>
+			<form className={styles.authScreen} onSubmit={(e) => handleSubmit(e)}>
+				<h2 className={styles.authScreenHeader}>
+					{registerMode ? 'Register' : 'Login'}
+				</h2>
 				<div className={styles.authScreenInput}>
-					<label htmlFor='email'>Email:</label>
+					<label htmlFor='username'>Username:</label>
 					<input
 						className={styles.authScreenInput}
-						value={email}
+						value={username}
 						type='text'
-						id='email'
-						onChange={(e) => {
-							setEmail(e.target.value);
-							setInvalidEmailError(false);
-						}}
+						id='username'
+						onChange={(e) => setUsername(e.target.value)}
 					/>
-					{invalidEmailError && (
-						<p className={styles.authScreenEmailError}>Invalid email</p>
-					)}
 				</div>
-			)}
-			<div className={styles.authScreenInput}>
-				<label htmlFor='password'>Password:</label>
-				<input
-					className={styles.authScreenInput}
-					value={password}
-					type={revealPassword ? 'text' : 'password'}
-					id='password'
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Eye
-					className={styles.authScreenEye}
-					title='reveal password'
-					onMouseDown={() => setRevealPassword(true)}
-					onMouseUp={() => setRevealPassword(false)}
-				/>
-			</div>
+				{registerMode && (
+					<div className={styles.authScreenInput}>
+						<label htmlFor='email'>Email:</label>
+						<input
+							className={styles.authScreenInput}
+							value={email}
+							type='text'
+							id='email'
+							onChange={(e) => {
+								setEmail(e.target.value);
+								setInvalidEmailError(false);
+							}}
+						/>
+						{invalidEmailError && (
+							<p className={styles.authScreenEmailError}>Invalid email</p>
+						)}
+					</div>
+				)}
+				<div className={styles.authScreenInput}>
+					<label htmlFor='password'>Password:</label>
+					<input
+						className={styles.authScreenInput}
+						value={password}
+						type={revealPassword ? 'text' : 'password'}
+						id='password'
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Eye
+						className={styles.authScreenEye}
+						title='reveal password'
+						onMouseDown={() => setRevealPassword(true)}
+						onMouseUp={() => setRevealPassword(false)}
+					/>
+				</div>
+				<button
+					disabled={isFetching ? true : false}
+					className={styles.authScreenButton}
+					type='submit'
+				>
+					{isFetching ? '...' : registerMode ? 'Register' : 'Login'}
+				</button>
+			</form>
 			<button
-				disabled={isFetching ? true : false}
-				className={styles.authScreenButton}
-				type='submit'
+				className={styles.authScreenLoginRegisterButton}
+				onClick={() => {
+					setRegisterMode(!registerMode);
+				}}
+				type='button'
 			>
-				{isFetching ? '...' : register ? 'Register' : 'Login'}
+				{registerMode ? 'Already have an account?' : `Don't have an account?`}
 			</button>
-		</form>
+		</>
 	);
 };
 export default AuthModal;
