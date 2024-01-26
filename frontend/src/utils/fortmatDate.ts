7ad7c4ca6064c21b8e009d11db1fdcb2fcc56d05
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-01-26 14:26:34 +0100

    refactor: date formatting util

--- frontend/src/utils/fortmatDate.ts
+++ frontend/src/utils/fortmatDate.ts
@@ -1,9 +1,13 @@
 export function formatDate(dateString: string): string {
-	return new Date(dateString).toLocaleString('en-US', {
-		year: 'numeric',
-		month: 'short',
-		day: 'numeric',
+	const options: Intl.DateTimeFormatOptions = {
+		year: '2-digit',
+		month: '2-digit',
+		day: '2-digit',
 		hour: 'numeric',
 		minute: 'numeric',
-	});
+	};
+
+	const formattedDate = new Date(dateString).toLocaleString('en-GB', options);
+
+	return formattedDate;
 }
